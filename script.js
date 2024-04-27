5d1d43a55bf2ea37f20c055cb1109730dc28d6ae
Joshua Watson <102503750+JoshLanderz19@users.noreply.github.com>
2024-04-27 02:48:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+function playPause(){
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+    } else {
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    }
+}
