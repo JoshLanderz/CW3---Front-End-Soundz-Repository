ca5af4322d82f69eb805e002f4d5f026a2584279
Joshua Watson <102503750+JoshLanderz19@users.noreply.github.com>
2024-04-29 17:10:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //Fresh On The Net - Play Button
+let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let playIcon = document.getElementById("playIcon");
+
+song.onloadedmetadata = function(){
+    song.pause();
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
 
 function playPause(){
     if(playIcon.classList.contains("fa-pause")){
@@ -14,6 +21,19 @@ function playPause(){
     }
 }
 
+if(song.play()){
+    setInterval(()=>{
+        progress.value = song.currentTime;
+    },500);
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    playIcon.classList.add("fa-pause");
+    playIcon.classList.remove("fa-play");
+}
+
 
 function searchOpen() {
     const defaultSearchEl = document.getElementById("searchEl");
@@ -24,6 +44,33 @@ function searchOpen() {
     const searchIcon = document.getElementById("search-bar-new-icon");
 
     searchIcon.style.right = "12px";
+}
+
+
+/*Navbar
+/*Navbar On Scroll*/
+window.onscroll = function() {navbarScroll()};
+
+function navbarScroll() {
+  const space = document.getElementById("space");
+  const navLinks = document.getElementById("nav-links");
+
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    space.classList.remove("space");
+    space.classList.add("space-scroll");
+  } else {
+    space.classList.add("space");
+    space.classList.remove("space-scroll");
+  }
+
+
+const recMottos = document.getElementById("motto-text");
+
+  if (document.body.scrollTop > 1330 || document.documentElement.scrollTop > 1330) {
+    recMottos.style.display = "flex";
+  } else {
+    recMottos.style.display = "none";
+  }
 }
 
 
